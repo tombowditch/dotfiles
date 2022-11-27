42ed54fc24f26fed9f20a21a2bbe87904d2ffdcc
Tommy Bowditch <me@tombowdit.ch>
2022-11-27 17:23:20 +0000

    add tab settings

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -2,3 +2,7 @@ vim.opt.relativenumber = true
 vim.opt.smartindent = true
 -- vim.g.mapleader = "<Space>"
 vim.g.mapleader = ","
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
