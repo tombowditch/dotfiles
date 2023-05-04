4fc115c7717595af19be45ec35e2b2ce06f14fb5
Tommy Bowditch <tommy.bowditch@nandos.co.uk>
2023-05-04 19:26:25 +0100

    colour

--- .config/nvim/after/plugin/colour.lua
+++ .config/nvim/after/plugin/colour.lua
@@ -1,105 +1,51 @@
-require("gruvbox").setup({
-  undercurl = true,
-  underline = true,
-  bold = true,
-  italic = {
-    strings = true,
-    comments = true,
-    operators = false,
-    folds = true,
+require('rose-pine').setup({
+  --- @usage 'auto'|'main'|'moon'|'dawn'
+  variant = 'main',
+  --- @usage 'main'|'moon'|'dawn'
+  dark_variant = 'main',
+  bold_vert_split = false,
+  dim_nc_background = false,
+  disable_background = false,
+  disable_float_background = false,
+  disable_italics = false,
+
+  --- @usage string hex value or named color from rosepinetheme.com/palette
+  groups = {
+    background = 'base',
+    background_nc = '_experimental_nc',
+    panel = 'surface',
+    panel_nc = 'base',
+    border = 'highlight_med',
+    comment = 'muted',
+    link = 'iris',
+    punctuation = 'subtle',
+
+    error = 'love',
+    hint = 'iris',
+    info = 'foam',
+    warn = 'gold',
+
+    headings = {
+      h1 = 'iris',
+      h2 = 'foam',
+      h3 = 'rose',
+      h4 = 'gold',
+      h5 = 'pine',
+      h6 = 'foam',
+    }
+    -- or set all headings at once
+    -- headings = 'subtle'
   },
-  strikethrough = true,
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  inverse = true,      -- invert background for search, diffs, statuslines and errors
-  contrast = "hard",   -- can be "hard", "soft" or empty string
-  overrides = {},
-  dim_inactive = false,
-  transparent_mode = false,
-  palette_overrides = {
-    dark0_hard = "#0A0E14",
-    dark0 = "#282828",
-    dark0_soft = "#32302f",
-    dark1 = "#00010A",
-    dark2 = "#504945",
-    dark3 = "#665c54",
-    dark4 = "#7c6f64",
-    light0_hard = "#f9f5d7",
-    light0 = "#fbf1c7",
-    light0_soft = "#f2e5bc",
-    light1 = "#ebdbb2",
-    light2 = "#d5c4a1",
-    light3 = "#bdae93",
-    light4 = "#a89984",
-    bright_red = "#fb4934",
-    bright_green = "#b8bb26",
-    bright_yellow = "#fabd2f",
-    bright_blue = "#83a598",
-    bright_purple = "#d3869b",
-    bright_aqua = "#8ec07c",
-    bright_orange = "#fe8019",
-    neutral_red = "#cc241d",
-    neutral_green = "#98971a",
-    neutral_yellow = "#d79921",
-    neutral_blue = "#458588",
-    neutral_purple = "#b16286",
-    neutral_aqua = "#689d6a",
-    neutral_orange = "#d65d0e",
-    faded_red = "#9d0006",
-    faded_green = "#79740e",
-    faded_yellow = "#b57614",
-    faded_blue = "#076678",
-    faded_purple = "#8f3f71",
-    faded_aqua = "#427b58",
-    faded_orange = "#af3a03",
-    gray = "#928374"
+
+  -- Change specific vim highlight groups
+  -- https://github.com/rose-pine/neovim/wiki/Recipes
+  highlight_groups = {
+    ColorColumn = { bg = 'rose' },
+
+    -- Blend colours against the "base" background
+    CursorLine = { bg = 'foam', blend = 10 },
+    StatusLine = { fg = 'love', bg = 'love', blend = 10 },
   }
 })
 
--- Theme: If you want to change the theme you need to
--- change this 2 things, colorscheme will apply the theme in the IDE
--- and you will also need to create a theme_colors to apply the theme
--- to the statusline and whichkey
-local colorscheme = "gruvbox"
-local colors = require("colours.gruvbox")
-
-local function set_colors(fg, bg)
-  return "guifg=" .. fg .. " guibg=" .. bg
-end
-
--- Applying colors
-local api = vim.api
-api.nvim_command("hi StatusDefault " .. set_colors(colors.fg, colors.bg))
-api.nvim_command("hi StatusVimNormal " .. set_colors(colors.bg, colors.accent))
-api.nvim_command("hi StatusVimInsert " .. set_colors(colors.bg, colors.neutral_blue))
-api.nvim_command("hi StatusVimVisual " .. set_colors(colors.bg, colors.func))
-api.nvim_command("hi StatusVimReplace " .. set_colors(colors.bg, colors.entity))
-api.nvim_command("hi StatusVimEnter " .. set_colors(colors.bg, colors.string))
-api.nvim_command("hi StatusVimMore " .. set_colors(colors.bg, colors.regexp))
-api.nvim_command("hi StatusVimSelect " .. set_colors(colors.bg, colors.markup))
-api.nvim_command("hi StatusVimCmd " .. set_colors(colors.bg, colors.keyword))
-api.nvim_command("hi StatusVimShell " .. set_colors(colors.bg, colors.special))
-api.nvim_command("hi StatusVimTerm " .. set_colors(colors.bg, colors.comment))
-api.nvim_command("hi StatusModified " .. set_colors(colors.bg, colors.constant))
-api.nvim_command("hi StatusLineNumber " .. set_colors(colors.bg, colors.operator))
-api.nvim_command("hi StatusColumnNumber " .. set_colors(colors.bg, colors.error))
-api.nvim_command("hi StatusFileInfo " .. set_colors(colors.keyword, colors.bg))
-api.nvim_command("hi StatusGitInfo " .. set_colors(colors.bright_green, colors.bg))
-api.nvim_command("hi StatusLSPProgress " .. set_colors(colors.neutral_blue, colors.bg))
-api.nvim_command("hi StatusLSPError " .. set_colors(colors.error, colors.bg))
-api.nvim_command("hi StatusLSPWarn " .. set_colors(colors.accent, colors.bg))
-api.nvim_command("hi StatusLSPInfo " .. set_colors(colors.entity, colors.bg))
-api.nvim_command("hi StatusLSPHin " .. set_colors(colors.tag, colors.bg))
-api.nvim_command("hi StatusLSPStatus " .. set_colors(colors.tag, colors.bg))
-api.nvim_command("hi StatusCwd " .. set_colors(colors.keyword, colors.bg))
-api.nvim_command("hi StatusCursor " .. set_colors(colors.accent, colors.bg))
-api.nvim_command("hi WhichKeyFloat " .. set_colors(colors.accent, colors.bg))
-api.nvim_command("hi SessionName " .. set_colors(colors.string, colors.bg))
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
-end
+vim.cmd('colorscheme rose-pine')
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,74 +1,79 @@
 require("lazy").setup({ -- plugins
--- {
---     "navarasu/onedark.nvim",
---     priority = 1000,
---     lazy = false,
---     config = function()
---         require('onedark').setup {
---             style = 'deep'
---         }
---         require('onedark').load()
---     end
--- },
-{"ellisonleao/gruvbox.nvim"}, {
-    "numToStr/Comment.nvim",
-    config = function()
-        require('Comment').setup()
-    end
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine'
+  },
+  -- {
+  --     "navarasu/onedark.nvim",
+  --     priority = 1000,
+  --     lazy = false,
+  --     config = function()
+  --         require('onedark').setup {
+  --             style = 'deep'
+  --         }
+  --         require('onedark').load()
+  --     end
+  -- },
+  { "ellisonleao/gruvbox.nvim" }, {
+  "numToStr/Comment.nvim",
+  config = function()
+    require('Comment').setup()
+  end
 }, {
-    "nvim-treesitter/nvim-treesitter",
-    run = ':TSUpdate'
+  "nvim-treesitter/nvim-treesitter",
+  run = ':TSUpdate'
 }, {
-    "nvim-telescope/telescope.nvim",
-    tag = '0.1.0',
-    dependencies = {"nvim-lua/plenary.nvim"}
-}, {"nvim-telescope/telescope-file-browser.nvim"}, {
-    "nvim-lualine/lualine.nvim",
-    dependencies = {"nvim-tree/nvim-web-devicons"}
+  "nvim-telescope/telescope.nvim",
+  tag = '0.1.0',
+  dependencies = { "nvim-lua/plenary.nvim" }
+}, { "nvim-telescope/telescope-file-browser.nvim" }, {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" }
 }, {
-    "windwp/nvim-autopairs",
-    config = function()
-        require("nvim-autopairs").setup {}
-    end
-}, {"glepnir/lspsaga.nvim"}, {
-    "akinsho/bufferline.nvim",
-    dependencies = {"nvim-tree/nvim-web-devicons"}
+  "windwp/nvim-autopairs",
+  config = function()
+    require("nvim-autopairs").setup {}
+  end
+}, { "glepnir/lspsaga.nvim" }, {
+  "akinsho/bufferline.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" }
 }, {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = {"nvim-tree/nvim-web-devicons"},
-    lazy = false
-    -- config = function()
-    --     require("nvim-tree").setup {}
+  "nvim-tree/nvim-tree.lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  lazy = false
+  -- config = function()
+  --     require("nvim-tree").setup {}
 
-    --     vim.api.nvim_create_autocmd({"VimEnter"}, {
-    --         callback = open_nvim_tree
-    --     })
-    -- end
-}, {"lewis6991/gitsigns.nvim"}, {"arkav/lualine-lsp-progress"}, {"wakatime/vim-wakatime"}, {"ThePrimeagen/vim-be-good"},
-{"nyoom-engineering/oxocarbon.nvim"}, {"dense-analysis/neural"}, {"MunifTanjim/nui.nvim"},
-{"ElPiloto/significant.nvim"}, {"akinsho/toggleterm.nvim"}, {
-    "VonHeikemen/lsp-zero.nvim",
-    dependencies = {"neovim/nvim-lspconfig", "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim",
-                    "hrsh7th/nvim-cmp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "saadparwaiz1/cmp_luasnip",
-                    "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-nvim-lua", "hrsh7th/cmp-cmdline", "L3MON4D3/LuaSnip",
-                    "rafamadriz/friendly-snippets"}
+  --     vim.api.nvim_create_autocmd({"VimEnter"}, {
+  --         callback = open_nvim_tree
+  --     })
+  -- end
+}, { "lewis6991/gitsigns.nvim" }, { "arkav/lualine-lsp-progress" }, { "wakatime/vim-wakatime" },
+  { "ThePrimeagen/vim-be-good" },
+  { "nyoom-engineering/oxocarbon.nvim" }, { "dense-analysis/neural" }, { "MunifTanjim/nui.nvim" },
+  { "ElPiloto/significant.nvim" }, { "akinsho/toggleterm.nvim" }, {
+  "VonHeikemen/lsp-zero.nvim",
+  dependencies = { "neovim/nvim-lspconfig", "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim",
+    "hrsh7th/nvim-cmp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "saadparwaiz1/cmp_luasnip",
+    "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-nvim-lua", "hrsh7th/cmp-cmdline", "L3MON4D3/LuaSnip",
+    "rafamadriz/friendly-snippets" }
 }, {
-    "folke/which-key.nvim",
-    config = function()
-        require("which-key").setup {}
-    end
-}, {"github/copilot.vim"}, {
-    "folke/trouble.nvim",
-    dependencies = {"nvim-tree/nvim-web-devicons"},
-    config = function()
-        require("trouble").setup {}
-    end
-}, {"simrat39/rust-tools.nvim"}, {
-    "windwp/nvim-spectre",
-    config = function()
-        require("spectre").setup {}
-    end
-}}, {
-    -- options
+  "folke/which-key.nvim",
+  config = function()
+    require("which-key").setup {}
+  end
+}, { "github/copilot.vim" }, {
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("trouble").setup {}
+  end
+}, { "simrat39/rust-tools.nvim" }, {
+  "windwp/nvim-spectre",
+  config = function()
+    require("spectre").setup {}
+  end
+} }, {
+  -- options
 
 })
