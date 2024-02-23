91a427525ba33dcbda4f2a128c004e287eecc5d4
Tommy Bowditch <me@tombowdit.ch>
2024-02-23 01:47:22 +0000

    zm

---
+++ .config/nvim/lua/plugins/zenmode.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      window = {
+        backdrop = 1,
+        height = 1,
+      },
+    },
+  },
+  {
+    "folke/twilight.nvim",
+    opts = {
+      treesitter = true,
+    },
+  },
+}
