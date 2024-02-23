8e38769f2df14b828b5c7d39c276da96808874d9
Tommy Bowditch <me@tombowdit.ch>
2024-02-23 01:53:13 +0000

    neogit

---
+++ .config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,21 @@
+return {
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim",
+		},
+
+		keys = {
+			{
+				"<leader>gn",
+				mode = { "n", "x", "o" },
+				function()
+					require("neogit").open()
+				end,
+				desc = "Open Neogit",
+			},
+		},
+		config = true,
+	},
+}
