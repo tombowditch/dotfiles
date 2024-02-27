663d389b364374f825735a01a92d64316d739714
Tommy Bowditch <tommy.bowditch@nandos.co.uk>
2024-02-27 10:21:26 +0000

    move ,gg to toggleterm.lua add rel ln toggle

--- .config/nvim/after/plugin/toggleterm.lua
+++ .config/nvim/after/plugin/toggleterm.lua
@@ -4,3 +4,7 @@ local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 function _LAZYGIT_TOGGLE()
 	lazygit:toggle()
 end
+
+vim.keymap.set("n", "<leader>gg", function()
+	_LAZYGIT_TOGGLE()
+end, { silent = true })
--- .config/nvim/lua/map.lua
+++ .config/nvim/lua/map.lua
@@ -7,10 +7,13 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz", o)
 vim.keymap.set("n", "n", "nzzzv", o)
 vim.keymap.set("n", "N", "Nzzzv", o)
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<leader>gg", function()
-	_LAZYGIT_TOGGLE()
-end, o)
 vim.keymap.set("x", "p", '"_dP', o)
+vim.keymap.set("n", "<leader>lnd", function()
+	vim.opt.relativenumber = false
+end, o)
+vim.keymap.set("n", "<leader>lne", function()
+	vim.opt.relativenumber = true
+end, o)
 
 --- Remove all trailing whitespace on save
 local TrimWhiteSpaceGrp = vim.api.nvim_create_augroup("TrimWhiteSpaceGrp", { clear = true })
