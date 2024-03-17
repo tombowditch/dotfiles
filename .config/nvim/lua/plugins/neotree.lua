3d65639607d84461814acc876a7c1029cf0a16d0
Tommy Bowditch <me@tombowdit.ch>
2024-03-17 14:19:55 +0000

    session

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -1,49 +1,41 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    branch = "v3.x",
-    -- https://github.com/LazyVim/LazyVim/blob/566049aa4a26a86219dd1ad1624f9a1bf18831b6/lua/lazyvim/plugins/editor.lua#L31C1-L38C9
-    -- init = function()
-    -- 	if vim.fn.argc() == 1 then
-    -- 		local stat = vim.loop.fs_stat(vim.fn.argv(0))
-    -- 		if stat and stat.type == "directory" then
-    -- 			require("neo-tree")
-    -- 		end
-    -- 	end
-    -- end,
-    cmd = "Neotree",
-    opts = {
-      close_if_last_window = false,
-      popup_border_style = "rounded",
-      enable_git_status = true,
-      enable_diagnostics = true,
-      filesystem = {
-        bind_to_cwd = true,
-        follow_current_file = {
-          enabled = true,
-        },
-        use_libuv_file_watcher = true,
-      },
-      default_component_configs = {
-        indent = {
-          with_expanders = true,
-          expander_collapsed = "",
-          expander_expanded = "",
-          expander_highlight = "NeoTreeExpander",
-        },
-      },
-    },
-    keys = {
-      {
-        ";t",
-        "<Cmd>Neotree toggle<CR>",
-        desc = "Toggle Neo-tree",
-      },
-    },
-  },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		branch = "v3.x",
+		cmd = "Neotree",
+		opts = {
+			auto_clean_after_session_restore = true,
+			close_if_last_window = false,
+			popup_border_style = "rounded",
+			enable_git_status = true,
+			enable_diagnostics = true,
+			filesystem = {
+				bind_to_cwd = true,
+				follow_current_file = {
+					enabled = true,
+				},
+				use_libuv_file_watcher = true,
+			},
+			default_component_configs = {
+				indent = {
+					with_expanders = true,
+					expander_collapsed = "",
+					expander_expanded = "",
+					expander_highlight = "NeoTreeExpander",
+				},
+			},
+		},
+		keys = {
+			{
+				";t",
+				"<Cmd>Neotree toggle<CR>",
+				desc = "Toggle Neo-tree",
+			},
+		},
+	},
 }
